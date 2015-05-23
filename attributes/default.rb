347b287919c0f1f0b587f198ed1fc19f78c3a1a3
Braulio Bhavamitra <braulio@eita.org.br>
2015-05-23 15:28:29 -0300

    Fix config for 5.6

--- attributes/default.rb
+++ attributes/default.rb
@@ -33,8 +33,10 @@ default[:mysql][:tunable][:max_heap_table_size]  = "32M"
 default[:mysql][:tunable][:myisam_recover]       = "BACKUP"
 default[:mysql][:tunable][:net_read_timeout]     = "30"
 default[:mysql][:tunable][:net_write_timeout]    = "30"
-default[:mysql][:tunable][:table_cache]          = "128"
-default[:mysql][:tunable][:table_open_cache]     = "128"
+if node[:mysql][:version] <= '5.5'
+  default[:mysql][:tunable][:table_cache]          = "128"
+  default[:mysql][:tunable][:table_open_cache]     = "128"
+end
 default[:mysql][:tunable][:thread_cache]         = "128"
 default[:mysql][:tunable][:thread_cache_size]    = 8
 default[:mysql][:tunable][:thread_concurrency]   = 10
@@ -44,7 +46,9 @@ default[:mysql][:tunable][:wait_timeout]         = "180"
 default[:mysql][:tunable][:query_cache_limit]    = "1M"
 default[:mysql][:tunable][:query_cache_size]     = "16M"
 
-default[:mysql][:tunable][:log_slow_queries]     = "/var/log/mysql/slow.log"
+if node[:mysql][:version] <= '5.5'
+  default[:mysql][:tunable][:log_slow_queries]     = "/var/log/mysql/slow.log"
+end
 default[:mysql][:tunable][:long_query_time]      = 2
 
 default[:mysql][:tunable][:innodb_buffer_pool_size] = "256M"
